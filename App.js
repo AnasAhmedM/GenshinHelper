a9339d277b7faccd91702e4f67cc3364b0afce3f
AnasAhmedM <anasnahmed.official@gmail.com>
2021-12-25 21:59:05 +0500

    Fixes

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+
 const loginComponent = require("./components/Login")
 const signupComponent = require("./components/Signup")
 const accountSettingsComponent = require("./components/AccountSettings")
@@ -16,17 +17,65 @@ const listChooseArtifactComponent = require("./components/ListChooseArtifact")
 const listChooseWeaponComponent = require("./components/ListChooseWeapon")
 const listChooseCharacterComponent = require("./components/ListChooseCharacter")
 
-import React from 'react';
+
+import {React, useState} from 'react';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
 
+
+
+
+function LoginScreen({ navigation, route}) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+ 
+  return (
+    <View style={styles.container}>
+    
+      <Icon name='person' color='black' size={256}/>
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Email."
+          placeholderTextColor="black"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+ 
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Password."
+          placeholderTextColor="black"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+ 
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}>Forgot Password?</Text>
+      </TouchableOpacity>
+ 
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='LoginScreen' screenOptions={{headerShown:false}}>
+        <Stack.Screen name='Login' component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -36,5 +85,40 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  image: {
+    marginBottom: 40,
+  },
+ 
+  inputView: {
+    backgroundColor: "gray",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+ 
+  textInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+ 
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+ 
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "gray",
   },
 });
--- components/Login.js
+++ components/Login.js
@@ -1,11 +1,43 @@
+import { React, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>Hello World</Text>
-        </View>
-    );
+    const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+ 
+  return (
+    <View style={styles.container}>
+    
+      <Icon name='person' color='black' size={256}/>
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Email."
+          placeholderTextColor="black"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+ 
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Password."
+          placeholderTextColor="black"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
+ 
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}>Forgot Password?</Text>
+      </TouchableOpacity>
+ 
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
