ce24959d1a477c257c89fc66bc2462d185afd28a
Ammar Malik <ammarrmalik185@hotmail.com>
2021-12-25 21:50:31 +0500

    components imported

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
+const loginComponent = require("./components/Login")
+const signupComponent = require("./components/Signup")
+const accountSettingsComponent = require("./components/AccountSettings")
+
+const listArtifactComponent = require("./components/ListArtifact")
+const listCharacterComponent = require("./components/ListCharacter")
+const listWeaponComponent = require("./components/ListWeapon")
+const listTeamComponent = require("./components/ListTeam")
+
+const singleArtifactComponent = require("./components/SingleArtifact")
+const singleWeaponComponent = require("./components/SingleWeapon")
+const singleCharacterComponent = require("./components/SingleCharacter")
+const singleTeamComponent = require("./components/SingleTeam")
+
+const listChooseArtifactComponent = require("./components/ListChooseArtifact")
+const listChooseWeaponComponent = require("./components/ListChooseWeapon")
+const listChooseCharacterComponent = require("./components/ListChooseCharacter")
+
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
   return (
